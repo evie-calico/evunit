d23f4d124bf4d7d1bba5f6498ae7bf4cd14cd90f
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2024-03-01 15:13:14 +0100

    Removed extra test script

--- test/test.sh
+++
@@ -1,7 +0,0 @@
-# build gb image if source is newer
-if [ "test/test.asm" -nt "test/test.gb" ]; then
-    rgbasm -o - test/test.asm | rgblink -o test/test.gb -n test/test.sym -
-fi
-
-# run evunit
-cargo run -- -c test/test.toml -d dump -n test/test.sym test/test.gb
